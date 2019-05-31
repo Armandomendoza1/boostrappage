9de0f98f0e0a9e740a2000bbfafea439139c5ab6
Armandoxx123 <josearmando.mendoza@itsn.edu.mx>
2019-05-31 09:26:48 -0500

    Pgina final

--- js/login.js
+++ js/login.js
@@ -12,3 +12,15 @@
           alert('usuario incorrecto');
         }
 }
+
+function ajax(){
+      var ajaxRes = new XMLHttpRequest()
+      ajaxRes.onreadystatechange = function(){
+        if (ajaxRes.readyState == 4 && ajaxRes.status == 200) {
+          document.getElementById("info").innerHTML=ajaxRes.responseText
+        }
+      }
+      ajaxRes.open("GET","registro.html",true)
+      //ajaxRes.open("GET","texto.txt",true)
+      ajaxRes.send()
+    }
